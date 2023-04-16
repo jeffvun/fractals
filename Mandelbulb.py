3f54cce7a6455082159d1ec0f7fe6c47f18f6519
jeffvun <vunangajoseph@gmail.com>
2023-04-17 00:04:43 +0300

    updating Mandelbulb

--- Mandelbulb.py
+++ Mandelbulb.py
@@ -2,47 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Define the maximum number of iterations
-MAX_ITERS = 20
+def mandelbulb(x, y, z, power, degree, max_iterations):
+    c = complex(x, y)
+    for i in range(max_iterations):
+        r = abs(c)
+        theta = np.arctan2(np.sqrt(x**2 + y**2), z)
+        phi = degree * np.arctan2(y, x)
+        c = (r**power) * complex(np.sin(theta*power)*np.cos(phi*power), np.sin(theta*power)*np.sin(phi*power))
+        c += complex(z, 0)
+        if abs(c) > 2:
+            return i
+    return max_iterations
 
-# Define the size of the grid
-GRID_SIZE = 100
+power = 8
+degree = 3
+max_iterations = 50
+num_points = 100
 
-# Define the size of the box
-BOX_SIZE = 2
+x = np.linspace(-1, 1, num_points)
+y = np.linspace(-1, 1, num_points)
+z = np.linspace(-1, 1, num_points)
 
-# Define the value of the exponent
-POWER = 8
+fractal = np.zeros((num_points, num_points, num_points))
 
-# Define the function to calculate the Mandelbulb
-def mandelbulb(z, power):
-    r = np.abs(z)
-    theta = np.arctan2(z.imag, z.real)
-    phi = np.arctan2(np.sqrt(z.real**2 + z.imag**2), z.imag)
-    new_r = r ** power
-    new_theta = power * theta
-    new_phi = power * phi
-    real_part = new_r * np.sin(new_phi) * np.cos(new_theta)
-    imag_part = new_r * np.sin(new_phi) * np.sin(new_theta)
-    return real_part + 1j * imag_part + z
+for i in range(num_points):
+    for j in range(num_points):
+        for k in range(num_points):
+            fractal[i,j,k] = mandelbulb(x[i], y[j], z[k], power, degree, max_iterations)
 
-# Define the function to calculate the Mandelbulb set
-def mandelbulb_set(xmin, xmax, ymin, ymax, zmin, zmax, grid_size, power, max_iters):
-    x, y, z = np.ogrid[xmin:xmax:grid_size*1j, ymin:ymax:grid_size*1j, zmin:zmax:grid_size*1j]
-    c = x + y*1j + z*1j
-    mask = np.ones(c.shape, dtype=bool)
-    z = c
-    for i in range(max_iters):
-        z = mandelbulb(z, power)
-        mask &= (np.abs(z) < 2)
-    return mask
-
-# Generate the Mandelbulb set
-mask = mandelbulb_set(-BOX_SIZE, BOX_SIZE, -BOX_SIZE, BOX_SIZE, -BOX_SIZE, BOX_SIZE, GRID_SIZE, POWER, MAX_ITERS)
-
-# Plot the Mandelbulb set
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-x, y, z = np.indices(mask.shape)
-ax.voxels(x, y, z, mask)
+ax.voxels(fractal, edgecolor='k')
+
+fig.set_size_inches(8, 8)
+plt.savefig('mandelbulb.png', dpi=300)
 plt.show()
